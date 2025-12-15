3a562b04e14a12bc367f0276a7cd4d6edd43c970
mellmanda <meam24rs@student.ju.se>
2025-12-15 11:12:12 +0100

    changed cloud colour

--- platforms.js
+++ platforms.js
@@ -1,57 +1,56 @@
-export default class Platform{
-    constructor(x, y, w, moving, dx, breaking){
-        this.x = x; 
-        this.y = y;
-        this.w = w;
-        this.h = 20;
-        this.moving = moving;
-        this.dx = dx;
-        this.scored = false;
-        this.breaking = breaking;
-        this.broken = false;
+export default class Platform {
+  constructor(x, y, w, moving, dx, breaking) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = 20;
+    this.moving = moving;
+    this.dx = dx;
+    this.scored = false;
+    this.breaking = breaking;
+    this.broken = false;
+  }
+
+  update() {
+    if (this.moving) {
+      this.x += this.dx;
+      if (this.x < 0 || this.x + this.w > width) this.dx *= -1;
+    }
+  }
+
+  tryLand(cat, prevBottom) {
+    if (this.broken) return { landed: false, scored: false, broke: false };
+
+    const catLeft = cat.x - cat.w / 2;
+    const catRight = cat.x + cat.w / 2;
+    const catBottom = cat.y + cat.h / 2;
+
+    if (
+      catLeft < this.x + this.w &&
+      catRight > this.x &&
+      prevBottom <= this.y &&
+      catBottom >= this.y &&
+      cat.vy > 0
+    ) {
+      // bounce
+      cat.y = this.y - cat.h / 2;
+      cat.vy = -10;
+
+      // scoring (platform remembers if it already scored)
+      const scoredNow = !this.scored;
+      if (scoredNow) this.scored = true;
+
+      // breaking platform
+      const brokeNow = this.breaking;
+      if (brokeNow) this.broken = true;
+
+      return { landed: true, scored: scoredNow, broke: brokeNow };
     }
 
-    update(){
-      if(this.moving){
-        this.x += this.dx;
-        if(this.x < 0 || this.x + this.w > width) this.dx *= -1;
-      }
-    }
+    return { landed: false, scored: false, broke: false };
+  }
 
-    tryLand(cat, prevBottom) {
-      if (this.broken) return { landed: false, scored: false, broke: false };
-
-      const catLeft = cat.x - cat.w / 2;
-      const catRight = cat.x + cat.w / 2;
-      const catBottom = cat.y + cat.h / 2;
-
-      if (
-        catLeft < this.x + this.w &&
-        catRight > this.x &&
-        prevBottom <= this.y &&
-        catBottom >= this.y &&
-        cat.vy > 0
-      ) {
-        // bounce
-        cat.y = this.y - cat.h / 2;
-        cat.vy = -10;
-
-        // scoring (platform remembers if it already scored)
-        const scoredNow = !this.scored;
-        if (scoredNow) this.scored = true;
-
-        // breaking platform
-        const brokeNow = this.breaking;
-        if (brokeNow) this.broken = true;
-
-        return { landed: true, scored: scoredNow, broke: brokeNow };
-      }
-
-      return { landed: false, scored: false, broke: false };
-    }
-
-
-    recycle(minY) {
+  recycle(minY) {
     this.x = random(0, width - this.w);
 
     const gapMin = 80;
@@ -59,18 +58,18 @@ export default class Platform{
     this.y = minY - random(gapMin, gapMax);
 
     this.dx = random([-1, 1]) * random(0.5, 1.2);
-    this.moving = random() <0.4;
+    this.moving = random() < 0.4;
     this.scored = false;
-    this.breaking = random() <0.2;
+    this.breaking = random() < 0.2;
     this.broken = false;
   }
 
   draw() {
     if (this.broken) return;
 
-    if (this.breaking){
-      fill(2);
-      stroke(130);
+    if (this.breaking) {
+      fill(170, 190, 210);
+      stroke(120, 140, 160);
     } else {
       fill(255);
       stroke(200);
